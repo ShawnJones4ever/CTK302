28711b0ebc977e6f986ac4404a04764f38556443
ShawnJones4ever <89145820+ShawnJones4ever@users.noreply.github.com>
2021-10-11 14:40:11 -0500

    last try

--- Unit2/MP2.0/sketch.js
+++ Unit2/MP2.0/sketch.js
@@ -41,21 +41,20 @@ function draw() {
     case 0:
       image(DONTOUCHME, 0, 0, 800, 800);
       text("THE NEXT PAGE IS SCARY, SO DONT TOUCH THAT MOUSE AT ALL", 100, 100);
+      textSize(20);
       timer++;
       if (timer > 200) {
         myState = 1;
         timer = 0;
       }
-
-
-
       break;
 
     case 1:
       image(scary, 0, 0, 800, 800);
       //background('green');
       text("WHAT THE........", 100, 100);
-
+      textSize(20);
+      textColor(white);
       //image(test, 0, 0, 800, 800);
       timer++;
       if (timer > 200) {
@@ -67,6 +66,8 @@ function draw() {
     case 2:
       image(omg, 0, 0, 800, 800);
       text("Please, Dont Do that again, for my sake", 100, 100);
+      textSize(20);
+      textColor(white);
       timer++;
       if (timer > 200) {
         myState = 3;
@@ -77,6 +78,8 @@ function draw() {
     case 3:
       image(scary, 0, 0, 800, 800);
       text("ahhhhhhhhhhhh.", 100, 100);
+      textSize(20);
+      textColor(white);
       timer++;
       if (timer > 400) {
         myState = 4;
@@ -87,6 +90,8 @@ function draw() {
     case 4:
       image(Stop, 0, 0, 800, 800);
       text("ARE YOU NOT READING THIS, I said DONT CLICK THE MOUSE....", 100, 100);
+      textColor(white);
+      textSize(20);
       timer++;
       if (timer > 200) {
         myState = 5;
@@ -97,6 +102,8 @@ function draw() {
     case 5:
       image(helpme, 0, 0, 800, 800);
       text("WHAT THE F....K!?!?!?!", 100, 100);
+      textColor(white);
+      textSize(20);
       timer++;
       if (timer > 200) {
         myState = 6;
@@ -107,6 +114,8 @@ function draw() {
     case 6:
       image(source, 0, 0, 800, 800);
       text("Alright CATCH THESE HANDS....", 100, 100);
+      textColor(white);
+      textSize(20);
       break;
 
   }
