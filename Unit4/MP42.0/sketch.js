13e5ac19b67453bc3d1952848c7565858ebb6bb0
ShawnJones4ever <89145820+ShawnJones4ever@users.noreply.github.com>
2021-12-06 00:13:33 -0600

    idk

--- Unit4/MP42.0/sketch.js
+++ Unit4/MP42.0/sketch.js
@@ -1,9 +1,14 @@
 let state = 0;
-let chicken;
+var chick;
 
 function setup() {
+
   createCanvas(500, 500);
-chicken = loadImage("assets/chicken.jpg");
+
+chick = loadImage("assets/chicken.jpg");
+rectMode(CENTER);
+ellipseMode(CENTER);
+imageMode(CENTER);
 
 function draw() {
 
@@ -11,12 +16,17 @@ function draw() {
 
     case 0:
       background('blue')
-      image(chicken, 0, 0, 500, 500);
+      image(chick,width/2, height/2, width, height);
+      textSize(100);
+      textAlign(CENTER);
       text("I went out dressed like a chicken last night and I met a girl who was dressed like an egg.One thing led to another and the lifelong question was answered:", width / 2, height / 2, 200, 200);
       break;
 
     case 1:
       background('yellow');
+      image(chick, width/2, height/2, width, height);
+      textSize(100);
+      textAlign(CENTER);
       text("it was the chicken.", width / 2, height / 2);
       break;
   }
